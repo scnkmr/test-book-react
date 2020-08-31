46ebb1b063be35745abaf156318ce50a7f26ce7a
sachin <sachinarayans@gmail.com>
2020-08-31 23:14:45 +0530

    question and option component printed whti api

--- src/App.js
+++ src/App.js
@@ -3,15 +3,52 @@ import './App.css';
 import Question from './Question';
 
 class App extends React.Component {
+  constructor(props){
+		super(props);
+		this.state = {
+			error: null,
+			isLoded: false,
+			questions: []
+		}
+	}
+	componentDidMount(){
+		fetch("http://localhost:81/test-book-backend/questions.php") //http://testbook.epizy.com/main.php?testid=2068
+		.then(res => res.json())
+		.then(
+			(result) => {
+				  this.setState({
+					isLoaded: true,
+					questions: result.questions
+				  });
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+				  this.setState({
+					isLoaded: true,
+					error
+				  });
+				}
+		)
+  }
   render(){
-	  return (
+    const { error, isLoaded, questions } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+    return (
     <div className="App">
       <div className="title">{this.props.title}</div>
-	  <Question/>
-	  <Question/>
+      {questions.map(question_react_var => (
+      <Question question={question_react_var.question} marks={question_react_var.question.marks} options={question_react_var.options}/>
+      ))}
     </div>
   );
   }
+    }
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -4,7 +4,7 @@ class Option extends React.Component{
 	render(){
 		return (
 			<div className="option_container">
-				<input type="radio"/><span className="option">option</span>
+				<input type="radio"/><span className="option">{this.props.optionText}</span>
 			</div>
 		);
 	}
@@ -12,54 +12,18 @@ class Option extends React.Component{
 class Question extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {
-			error: null,
-			isLoded: false,
-			questions: []
-		}
-	}
-	componentDidMount(){
-		fetch("http://localhost:81/test-book/questions.php") //http://testbook.epizy.com/main.php?testid=2068
-		.then(res => res.json())
-		.then(
-			(result) => {
-				  this.setState({
-					isLoaded: true,
-					questions: result.questions
-				  });
-				},
-				// Note: it's important to handle errors here
-				// instead of a catch() block so that we don't swallow
-				// exceptions from actual bugs in components.
-				(error) => {
-				  this.setState({
-					isLoaded: true,
-					error
-				  });
-				}
-		)
 	}
   render(){
-	  const { error, isLoaded, questions } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-		console.log(questions[0].question);
 	  return (
 	  <div className="question_group">
-	  {questions.map(questions_react_var => (questions_react_var.question))}
 	  <div className="question_container">
-		Questions<span className="points"> 2 Points</span>
+		{this.props.question}<span className="points">{this.props.marks}</span>
 	  </div>
-		<Option/>
-		<Option/>
+			{this.props.options.map(option => (<Option optionText={option}/>))}
 		<button>Clear selection</button>
 	  </div>
 		);
 	}
-  }
 }
 
 export default Question;
